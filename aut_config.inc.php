2c8c3bbd51107ba6e834e806b9d785d5b7667b82
juan <jvg@web.com>
2021-02-19 23:18:05 -0600

    pull de branch gh-pages

--- aut_config.inc.php
+++ aut_config.inc.php
@@ -3,7 +3,7 @@
 $usuarios_sesion="puntoDeVenta";
 
 // Datos conexi�n a la Base de datos (MySql)
-$sql_host="localhost";  // Host, nombre del servidor o IP del servidor Mysql.
+$sql_host="";  // Host, nombre del servidor o IP del servidor Mysql.
 $sql_usuario="publico2_uvb";        // Usuario de Mysql
 $sql_pass="12**//uvb3";           // contrase�a de Mysql
 $sql_db="publico2_pv_m";     // Base de datos que se usar�.
